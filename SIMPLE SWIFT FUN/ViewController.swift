3d1fc524b49701b0ce7e4869063ca45525cb6e06
Abhi <abhi@abhisheks-macbook-pro.local>
2018-03-17 21:13:15 +0530

    Added some more control

--- SIMPLE SWIFT FUN/ViewController.swift
+++ SIMPLE SWIFT FUN/ViewController.swift
@@ -13,13 +13,28 @@ class ViewController: UIViewController {
     var buttonTappedCount = 0
     @IBOutlet weak var headline: UILabel!
     
+    @IBOutlet weak var inputOne: UITextField!
     
+    @IBOutlet weak var inputTwo: UITextField!
+    
+    
+    
+    @IBOutlet weak var onLightMode: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
-        view.backgroundColor = #colorLiteral(red: 0.9921568627, green: 0.7176470588, blue: 0.007843137255, alpha: 1)
-        headline.text = "Abhi is cool"
+        view.backgroundColor = #colorLiteral(red: 0.968627451, green: 0.968627451, blue: 0.968627451, alpha: 1)
+        
+        
+        
+    }
+    
+    @IBAction func EnableNightMode(_ sender: UISwitch) {
+        if onLightMode.isOn{
+            view.backgroundColor = #colorLiteral(red: 0.1137254902, green: 0.1137254902, blue: 0.1137254902, alpha: 1)
+        headline.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
     }
     
     
@@ -30,6 +45,15 @@ class ViewController: UIViewController {
             view.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
         }
         
+        let val1: Int = Int(inputOne.text!)!
+        let val2: Int = Int(inputTwo.text!)!
+        
+        print(val1)
+        print(val2)
+        
+        let ans = val1 + val2
+        
+        headline.text = String(ans)
     }
     
     override func didReceiveMemoryWarning() {
